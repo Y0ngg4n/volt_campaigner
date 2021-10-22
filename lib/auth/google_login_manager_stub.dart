05587cd4f0f26e7cae6ff6ef53236521a0be8c63
Yonggan <yonggan@oblivioncoding.pro>
2021-10-22 20:38:57 +0200

    Backup

---
+++ lib/auth/google_login_manager_stub.dart
@@ -0,0 +1,4 @@
+import 'package:volt_campaigner/auth/google_login_manager.dart';
+
+GoogleLoginManager getManager(OnLogin onLogin) =>
+    throw UnsupportedError('Cannot create a cross platform Login');
